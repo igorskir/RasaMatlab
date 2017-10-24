55861e6006f5e4dd814e80212b995ed57b365354
Igor Skirnevskiy <igorskir@yandex.ru>
2017-10-24 12:46:12 +0700

    Доделал функцию GetIntesection

--- GetIntersection.m
+++ GetIntersection.m
@@ -10,6 +10,7 @@ function overlap = GetIntersection(v1, v2, varargin)
     
     defEpsilon = 0.01;
     overlap = 0;
+    possibleCombs = numel(v1)*numel(v2);
     
     switch nargin
         case 2
@@ -20,15 +21,20 @@ function overlap = GetIntersection(v1, v2, varargin)
             disp('Unexpected inputs');
             
     end
+    
+    % sort array elemnts for algorithms optimization
+    v1 = sort(v1);
+    v2 = sort(v2);
 
     for i = 1:numel(v1)
        for j = 1:numel(v2)           
-            minVal = min(v1(i), v2(j));
-            maxVal = max(v1(i), v2(j));
-            if (1 - (minVal/maxVal)) < epsilon
-                overlap = overlap + 1;            
+            maxVal = max(v1(i), v2(j));         
+            if ( abs(v1(i) - v2(j))/maxVal ) < epsilon
+                overlap = overlap + 1;   
+            else
+               break; 
             end
        end
     end
-    overlap = (overlap/(numel(v1)*numel(v2)))*100;
+    overlap = (overlap/possibleCombs)*100;
 end
