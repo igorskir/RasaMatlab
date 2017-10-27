d0bc0931df8f0608782aba3f122175eefff2f2d3
TPU\ram8 <ram290495@gmail.com>
2017-10-27 16:56:35 +0700

    Добавлена функция для построения сглаженной PDF

---
+++ MLForCatheterDetection/GetPDFSpline.m
@@ -0,0 +1,31 @@
+function [ pp ] = GetPDFSpline(data,binCount)
+maxEl=max(data);
+minEl=min(data);
+%binCount=10;
+step=(maxEl-minEl)/binCount;
+edges=minEl:step:maxEl;
+bins=zeros(binCount,1);
+
+for i=1:numel(data)
+    for j=1:binCount
+        currEl=data(i);
+        if(currEl>=edges(j) && currEl<=edges(j+1))
+            bins(j)=bins(j)+1;
+            break;
+        end
+    end
+end
+bins=bins/(numel(data)*step);
+middles=zeros(binCount,1);
+for i=1:binCount
+    middles(i)=(edges(i)+edges(i+1))/2;
+end
+figure;
+plot(middles,bins);
+hold on;
+x=minEl:step*0.01:maxEl;
+y=spline(middles,bins,x);
+maxEl
+
+plot(x,y,'red');
+end
