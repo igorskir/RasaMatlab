a6d06ff714d5e566df311601c5d7e118d5d5dae3
Viacheslav V. Danilov <viacheslav.v.danilov@gmail.com>
2017-12-24 13:18:13 +0100

    KNN feature selection

--- MLForCatheterDetection/featureSelection.m
+++ MLForCatheterDetection/featureSelection.m
@@ -5,7 +5,7 @@ addpath(genpath(pwd));
 clear currentFolder;
 
 useNormalizedData = 0; % 1 - yes, 0 - no (as IP said default value is euqal to 0)
-funType = 2; % 1 - dicriminant, 2 - svm, 3 - knn, 4 - fsra
+funType = 3; % 1 - dicriminant, 2 - svm, 3 - knn, 4 - fsra
 cvType = 1; % 1 - k-fold, 2 - Holdout
 featType = 1; % 1 - auto, 2 - 6 featues, 3 - 12 features
 options = statset('display', 'iter', 'MaxIter', 1000);
